404df277d3f612fe3adfc7cfe4aee043c9a1bfdf
Nils August Jakobsen <nilsaj@uia.no>
2025-08-22 12:26:57 +0200

    complete output

--- assignment_1/main.py
+++ assignment_1/main.py
@@ -2,6 +2,14 @@ import cv2
 import numpy as np
 def print_image_information(image ):
 
+    width,height,channels = image.shape
+
+
+    print("height", height)
+    print("width",width)
+    print("channels",channels)
+    print("size",image.size)
+    print("data type",image.dtype)
     pass
 
 def main():
@@ -9,6 +17,7 @@ def main():
     img =cv2.imread("lena.png")
     cv2.imshow("image",img)
     cv2.waitKey(0)
+    print_image_information(img)
 
 if __name__ ==  "__main__":
     main()

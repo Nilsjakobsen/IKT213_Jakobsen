8c681f8761991d6e85d6f401839c6f4a56b788f8
Nils August Jakobsen <nilsaj@uia.no>
2025-08-25 13:56:53 +0200

    working on assignmet 2

--- assignment_2/main.py
+++ assignment_2/main.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 import cv2
 
 
@@ -5,13 +6,52 @@ import cv2
 
 
 
-def padding():
-    image=
-    border_width=jnnj
+def padding(image, border_width):
+
+    image_padded= cv2.copyMakeBorder( image, border_width,border_width,border_width,border_width,cv2.BORDER_REFLECT)
+
+    return  image_padded
+
+def crop(image, x_0,x_1,y_0,y_1):
+    cropped_image = image[y_0:y_1, x_0:x_1]
+
+    return cropped_image
+
+def resize(image, width, height):
+    resized_image = cv2.resize(image, (width,   height))
+
+    return resized_image
+
+def copy(image, emptyPictureArray):
+    emptyPictureArray = [:]
+    height, width, channels = image.shape
+    height=
+    width=
+    channels=
+
+def main():
+    img = cv2.imread("lena.png")
+#task 1:
+    border= padding(img,100)
+    cv2.imwrite("solutions/lena_reflected.png",border)
+
+
+#task 2:
+    image_cropped= crop(img, 80, 512-130,80,512-130)
+    cv2.imwrite("solutions/lena_cropped.png",image_cropped )
+
+#task 3:
+    resized =resize(img,200,200)
+    cv2.imwrite("solutions/lena_resized.png",resized)
+
+#task 4
 
 
 
 
+cv2.imwrite("solutions/lena_copy.png",    )
+
+if __name__ ==  "__main__":
+    main()
 
 
-img =cv2.imread("lena.png")
